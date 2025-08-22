1ba5bf338ee4d6b8dac2a2601b224fdb0e402d43
Jesmine <tingziching@gmail.com>
2025-08-22 23:22:49 +0800

    feat: updated the heart-3d-section to the latest design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,8 +58,6 @@ export default function Home() {
         {/* Hero Section - Main introduction with The Challenge and Our Solution */}
         <HeroSection />
 
-
-
         {/* FAQ Section - Frequently Asked Questions */}
         <FaqSection />
         
